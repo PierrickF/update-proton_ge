b6e3d65b8678f7ae1b89e85a989236875f9859da
PierrickF <pierrickfoulquier@gmail.com>
2024-02-16 10:54:49 +0100

    Add documentation for CompareVersions()

--- update_proton_ge/update_proton_ge.go
+++ update_proton_ge/update_proton_ge.go
@@ -1,5 +1,7 @@
 package update_proton_ge
 
+// CompareVersions takes in two strings and compare them.
+// If they are equal, the software is up to date.
 func CompareVersions(latest, local string) bool {
 	return latest == local
 }
