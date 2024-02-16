50d20dca31bbb52adcb2d4140b4bbcf62345cb60
PierrickF <pierrickfoulquier@gmail.com>
2024-02-16 19:13:36 +0100

    Add ParseGithubData + test

--- update_proton_ge/update_proton_ge.go
+++ update_proton_ge/update_proton_ge.go
@@ -1,79 +1,41 @@
 package update_proton_ge
 
 import (
-	"log"
+	"encoding/json"
+	"fmt"
 	"os/exec"
 )
 
-// GetLatest runs shell commands to fetch a URL and strip it down to a version name.
+func ParseGithubData(data []byte) (string, error) {
+	var release struct {
+		ReleaseTag string `json:"tag_name"`
+	}
+	err := json.Unmarshal(data, &release)
+	if err != nil {
+		return "", err
+	}
+
+	return release.ReleaseTag, nil
+}
+
+// GetLatest runs shell commands to fetch a URL
+
+// and strip it down to a version name.
 // The output of the shell commands is captured in a slice of bytes,
 // which is then converted to a string.
-func GetLatest() string {
+func GetLatest() (string, error) {
 
 	// Define shell commands
 	curlUrl := "https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest"
 	curl := exec.Command("curl", "-s", curlUrl)
-	grep := exec.Command("grep", "-E", "browser_download_url.*.tar.gz")
-	cut1 := exec.Command("cut", "-d", "\"", "-f4")
-	cut2 := exec.Command("cut", "-d", "/", "-f8")
 
-	// Set up pipes between commands
-	curlOutput, err := curl.StdoutPipe()
+	curlOutput, err := curl.Output()
 	if err != nil {
-		log.Fatalf("Error piping curl to grep: %s", err)
-	}
-	grep.Stdin = curlOutput
-
-	grepOutput, err := grep.StdoutPipe()
-	if err != nil {
-		log.Fatalf("Error piping grep to cut: %s", err)
-	}
-	cut1.Stdin = grepOutput
-
-	cut1Output, err := cut1.StdoutPipe()
-	if err != nil {
-		log.Fatalf("Error piping cut to cut: %s", err)
-	}
-	cut2.Stdin = cut1Output
-
-	// Start commands
-	err = curl.Start()
-	if err != nil {
-		log.Fatalf("Error starting curl: %s", err)
+		return "", fmt.Errorf("Curl error: %w", err)
 	}
 
-	err = grep.Start()
-	if err != nil {
-		log.Fatalf("Error starting grep: %s", err)
-	}
+	return ParseGithubData(curlOutput)
 
-	err = cut1.Start()
-	if err != nil {
-		log.Fatalf("Error starting cut: %s", err)
-	}
-
-	// Start, retreive output, and terminate command
-	latest, err := cut2.Output()
-	if err != nil {
-		log.Fatalf("Error running cut: %s", err)
-	}
-
-	// Terminate commands
-	err = curl.Wait()
-	if err != nil {
-		log.Fatalf("Error terminating curl: %s", err)
-	}
-
-	err = grep.Wait()
-	if err != nil {
-		log.Fatalf("Error terminating grep: %s", err)
-	}
-
-	err = cut1.Wait()
-	if err != nil {
-		log.Fatalf("Error terminating cut: %s", err)
-	}
-	return string(latest)
 }
 
 // GetLocal runs shell commands to find a file name.
--- update_proton_ge/update_proton_ge_test.go
+++ update_proton_ge/update_proton_ge_test.go
@@ -2,18 +2,31 @@ package update_proton_ge_test
 
 import (
 	"main/update_proton_ge"
+	"os"
 	"os/exec"
 	"testing"
 )
 
-func TestGetLatest(t *testing.T) {
+func TestParseGithubDataReturnsCorrectVersionName(t *testing.T) {
 	t.Parallel()
 
-	got := update_proton_ge.GetLatest()
+	data, err := os.ReadFile("../testdata/gh-data.json")
 
-	if got == "" {
-		t.Error("Want a version name, got an empty string")
+	if err != nil {
+		t.Fatal(err)
 	}
+
+	want := "GE-Proton8-32"
+	got, err := update_proton_ge.ParseGithubData(data)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if want != got {
+		t.Errorf("Want %q, got %q", want, got)
+	}
+
 }
 
 func TestGetLocal(t *testing.T) {
