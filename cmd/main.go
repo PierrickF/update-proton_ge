25812079fd3535495ed6ba80b6d3e4576c00dca5
PierrickF <pierrickfoulquier@gmail.com>
2024-02-26 02:00:50 +0100

    Populate main() with current functions

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,18 @@
 package main
 
-import "update_proton_ge"
+import (
+	"os"
+	"update_proton_ge"
+)
 
 func main() {
+	installDir := "~/.steam/root/compatibilitytools.d/"
+
+	releaseData, _ := update_proton_ge.DownloadReleaseData()
+	latestRelease, _ := update_proton_ge.ParseReleaseData(releaseData)
+	latestInstall, _ := update_proton_ge.FindLatestInstall(installDir)
+	if !update_proton_ge.NewerReleaseIsAvailable(latestRelease, latestInstall) {
+		println("Already up to date, nothing to do.")
+		os.Exit(0)
+	}
 }
