14fb19f7b2a73512ea868e1097b928e30e8756f6
PierrickF <pierrickfoulquier@gmail.com>
2024-02-29 05:35:29 +0100

    Refactor ParseReleaseData to return a release tag or a URL

--- parse_release_data.go
+++ parse_release_data.go
@@ -2,19 +2,42 @@ package update_proton_ge
 
 import (
 	"encoding/json"
+	"errors"
 )
 
-// ParseReleaseData takes in JSON data downloaded from GitHub,
-// and parses it to retreive the release name of Proton GE,
-// which is then returned.
-func ParseReleaseData(data []byte) (name string, err error) {
-	var release struct {
-		ReleaseTag string `json:"tag_name"`
+// ParseReleaseData takes in JSON data downloaded from GitHub and a type of
+// target to look for, and parses the data to retreive and return one of the
+// following:
+// - the release tag of Proton GE
+// - the URL to the tarball
+// - the URL to the checksum
+func ParseReleaseData(data []byte, targetType string) (target string, err error) {
+
+	type asset struct {
+		Name string `json:"name"`
+		Url  string `json:"browser_download_url"`
 	}
+
+	var release struct {
+		ReleaseTag string  `json:"tag_name"`
+		TarballUrl string  `json:"tarball_url"`
+		Assets     []asset `json:"assets"`
+	}
+
 	err = json.Unmarshal(data, &release)
+
 	if err != nil {
 		return "", err
 	}
 
-	return release.ReleaseTag, nil
+	switch targetType {
+	case "releaseTag":
+		return release.ReleaseTag, nil
+	case "tarballUrl":
+		return release.TarballUrl, nil
+	case "checksumUrl":
+		return release.Assets[0].Url, nil
+	default:
+		return "", errors.New("invalid target type")
+	}
 }
