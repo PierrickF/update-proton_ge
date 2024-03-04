e50f2cedcafd41b3cb6de7ee41f2daae1068d9fb
PierrickF <pierrickfoulquier@gmail.com>
2024-03-04 19:48:53 +0100

    Update permissions to 0777 because of umask

--- make_temp_directory.go
+++ make_temp_directory.go
@@ -3,7 +3,7 @@ package update_proton_ge
 import "os"
 
 func MakeTempDirectory() error {
-	err := os.Mkdir("/tmp/update-proton-ge/", 0660)
+	err := os.Mkdir("/tmp/update-proton-ge/", 0777)
 
 	if err != nil {
 		return err
